01c51fb3a6a41c9b087b09adee93c8a27de28dc7
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-07-27 01:06:52 +0900

    Add files via upload

--- MATRIX.py
+++ MATRIX.py
@@ -1,25 +1,34 @@
-"""
-D=(a*d-b*c)%mod
-x,y,z=sympy.gcdex(D,mod)
-x=x%mod
-print(x)
-P=sympy.Matrix([[3,7,4],[9,2,6]])
-
-"""
-
 import sympy
 import numpy
 
 def main():
     switch=input('暗号化鍵で暗号化 -> 1  /  暗号化鍵で複合化 -> 2  /  鍵解読 (2x2の暗号文と平文が必要) -> 3\n')
     if switch == '1':
-        key,mod=setting()
-        print('')
+        mod=26# アルファベットのみなので thkkeykwow
+        print('暗号化に使用する鍵を設定')
+        key=setting()
+        print('暗号化したい、アルファベットの平文を設定')
+        P,m=multi()
+        C=(key*P)%mod
+        out(C,m)
     elif switch == '2':
-        print('暗号文')
-        reading()
+        mod=26
+        print('暗号化に使用する鍵を設定')
+        key=setting()
+        kinv=key.inv_mod(mod)
+        print('暗号化したい、アルファベットの暗号化された文を設定')
+        C,m=multi()
+        P=(kinv*C)%mod
+        out(P,m)
     elif switch == '3':
-        print('暗号文')
+        print('暗号文の入力')
+        C=setting()
+        print('平文の入力')
+        P=setting()
+        mod=int(input('mod='))
+        Pinv=P.inv_mod(mod)
+        key=(C*Pinv)%mod
+        print(key)
     else :
         print('')
 
@@ -30,11 +39,50 @@ def setting():
     b=int(input('b='))
     c=int(input('c='))
     d=int(input('d='))
-    mod=int(input('mod='))
     k=sympy.Matrix([[a,b],[c,d]])
-    return k,mod
+    return k
 
-def reading():
+def line():
+    print('[[a,b],[c,d]](mod n)')
+    a=str(input('a='))
+    b=str(input('b='))
+    c=str(input('c='))
+    d=str(input('d='))
+    a=ord(a)-97
+    b=ord(b)-97
+    c=ord(c)-97
+    d=ord(d)-97
+    k=sympy.Matrix([[a,b],[c,d]])
+    return k
+
+def multi():
+    a1=str(input('文字入力、'))
+    a2=([])
+    for j in a1:
+        aord=ord(j)-96
+        a2.append(aord)
+    n=int(len(a2))
+    if (n%2)==1:
+        a2.append(0)
+    m=(len(a2)//2)# 2行m列
+    a=numpy.array(a2)
+    b=a.reshape(m,2)
+    c=b.T
+    A=sympy.Matrix(c)
+    return A,m
+
+def out(A:sympy.matrices.dense.MutableDenseMatrix , m:int):
+    output=[]
+    for x in range(m):
+        for y in range(2):
+            ins=A[y,x]+96
+            if ins!=96:    
+                ins=chr(ins)
+                output.append(ins)
+    output="".join(output)
+    print(output)
+
+def prerelease():
     m1=numpy.array([[2,4,56,2,3]])
     print(type(m1),m1.dtype)
     while(1):
@@ -44,17 +92,7 @@ def reading():
         print(type(m2))
         if len(a)%2==1:
             print(m2)
-        
-        
         break
 
-"""
-def decrypt():
-    k_inv=k.inv_mod(mod)#何故かこれは正常に動く。
-    print(k)
-    C=k_inv*P
-    print(C)
-
-"""
 if __name__ == '__main__':
     main()
